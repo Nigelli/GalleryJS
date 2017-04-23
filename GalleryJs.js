08fd8912b8fe9d005a07b39b4982d45c7e8aa710
Nigel Anderson <nigelnigelanderson@gmail.com>
2017-04-23 22:22:59 +0100

    Initial Commit ES6 non compiled

---
+++ GalleryJs.js
@@ -0,0 +1,85 @@
+// GalleryJs
+
+// Image should be passed into the Gallery as an object {string:id, string:src, optional string:alt}
+// When initializing the Gallery an array off thumbnails can be passed in to the Gallery
+
+/* TODO:
+  1: add and remove loading image for thumbnails and images
+  2:
+*/
+
+{
+  class Gallery {
+    constructor(images = [], thumbnails = []) {
+      this._PreviewArea = this.getPreviewArea();
+      this._ButtonArea = this.getButtonArea();
+      this._SliderArea = this.getSliderArea();
+      if (!this._PreviewArea || !this._ButtonArea || !this._SliderArea) {
+        return error('Failed to initialize Gallery, Check that you have included the required elements');
+      }
+
+      this.addThumbnails(thumbnails);
+      this.addImages(images);
+    }
+    // The three areas required in order to initialize the Gallery
+    getPreviewArea() {
+      return document.getElementById('#galleryPreviewArea');
+    }
+    getButtonArea() {
+      return document.getElementById('#galleryButtonArea');
+    }
+    getSliderArea() {
+      return document.getElementById('#gallerySliderArea');
+    }
+
+    addThumbnails(thumbnails) {
+      thumbnails.forEach((thumbnail) => {
+        this.setThumbnail(thumbnail);
+      })
+    }
+
+    addImages(images) {
+      images.forEach((image) => {
+        this.setImage(image);
+      })
+    }
+
+    setThumbnail(image) {
+      let alt = !image.alt ? "" : image.alt;
+      if (!image.src) {
+        return error("image source not provided", image)
+      }
+      if (!image.id) {
+        return error("image id not provided", image)
+      }
+      let imgThumb = $(`<img src="${image.src}" id="thumb-${image.id}" alt="${alt}" class="gallery-thumbnail hidden">`)
+      $(imgThumb).load(() => {
+        $(this._PreviewArea).append(imgThumb);
+      })
+    }
+
+    setImage(image) {
+      let alt = !image.alt ? "" : image.alt;
+      if (!image.src) {
+        return error("image source not provided", image)
+      }
+      if (!image.id) {
+        return error("image id not provided", image)
+      }
+      let imgThumb = $(`<img src="${image.src}" id="thumb-${image.id}" alt="${alt}" class="gallery-image hidden">`)
+      $(imgThumb).load(() => {
+        $(this._PreviewArea).append(imgThumb);
+      })
+    }
+
+    error(err, object) {
+      if (object) {
+        console.log(err);
+        console.log(object;
+      } else {
+        console.log(err);
+      }
+    }
+  }
+
+}
